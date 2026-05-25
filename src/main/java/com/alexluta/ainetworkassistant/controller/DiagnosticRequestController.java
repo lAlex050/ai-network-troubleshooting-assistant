8c19248ac30401ef61fb31eb791e72512465fa6c
lAlex050 <153731070+lAlex050@users.noreply.github.com>
2026-05-25 15:54:01 +0300

    Implement diagnostic request creation and user diagnostic history endpoints

---
+++ src/main/java/com/alexluta/ainetworkassistant/controller/DiagnosticRequestController.java
@@ -0,0 +1,36 @@
+package com.alexluta.ainetworkassistant.controller;
+
+import com.alexluta.ainetworkassistant.dto.request.CreateDiagnosticRequestDto;
+import com.alexluta.ainetworkassistant.dto.response.DiagnosticResponse;
+import com.alexluta.ainetworkassistant.entity.DiagnosticRequest;
+import com.alexluta.ainetworkassistant.service.DiagnosticRequestService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/")
+public class DiagnosticRequestController {
+
+    public final DiagnosticRequestService diagnosticRequestService;
+
+    public DiagnosticRequestController(DiagnosticRequestService diagnosticRequestService){
+        this.diagnosticRequestService = diagnosticRequestService;
+    }
+
+    @PostMapping("/users/{userId}/diagnostics")
+    public ResponseEntity<DiagnosticResponse> addDiagnosticByUserId(@PathVariable Long userId,
+                                                                    @Valid @RequestBody CreateDiagnosticRequestDto dto){
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(diagnosticRequestService.addDiagnosticByUserId(userId, dto));
+    }
+
+    @GetMapping("/users/{userId}/diagnostics")
+    public ResponseEntity<List<DiagnosticResponse>> getDiagnosticsByUserId(@PathVariable Long userId){
+        return ResponseEntity.status(HttpStatus.OK).body(diagnosticRequestService.getAllDiagnosticsByUserId(userId));
+    }
+
+}
---
+++ src/main/java/com/alexluta/ainetworkassistant/service/DiagnosticRequestService.java
@@ -0,0 +1,58 @@
+package com.alexluta.ainetworkassistant.service;
+
+
+import com.alexluta.ainetworkassistant.dto.request.CreateDiagnosticRequestDto;
+import com.alexluta.ainetworkassistant.dto.response.DiagnosticResponse;
+import com.alexluta.ainetworkassistant.entity.DiagnosticRequest;
+import com.alexluta.ainetworkassistant.entity.User;
+import com.alexluta.ainetworkassistant.enums.SeverityLevel;
+import com.alexluta.ainetworkassistant.exception.UserNotFoundException;
+import com.alexluta.ainetworkassistant.mapper.DiagnosticRequestMapper;
+import com.alexluta.ainetworkassistant.repository.DiagnosticRequestRepository;
+import com.alexluta.ainetworkassistant.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DiagnosticRequestService {
+
+    public final DiagnosticRequestRepository diagnosticRequestRepository;
+    public final UserRepository userRepository;
+    private final DiagnosticRequestMapper diagnosticRequestMapper;
+
+    public DiagnosticRequestService(DiagnosticRequestRepository diagnosticRequestRepository, UserRepository userRepository, DiagnosticRequestMapper diagnosticRequestMapper){
+        this.diagnosticRequestRepository = diagnosticRequestRepository;
+        this.userRepository = userRepository;
+        this.diagnosticRequestMapper = diagnosticRequestMapper;
+    }
+
+    public DiagnosticResponse addDiagnosticByUserId (Long userId, CreateDiagnosticRequestDto diagnosticRequestDto){
+        User user = userRepository.findById(userId)
+                .orElseThrow((() -> new UserNotFoundException(userId)));
+
+        DiagnosticRequest diagnostic = diagnosticRequestMapper.toEntity(diagnosticRequestDto);
+        diagnostic.setUser(user);
+        // mock uri pentru ca nu avem inca ai integrat
+        diagnostic.setAiResponse("Possible routing issue detected.");
+        diagnostic.setSeverity(SeverityLevel.MEDIUM);
+        return diagnosticRequestMapper.toDto(diagnosticRequestRepository.save(diagnostic));
+
+    }
+
+
+    public List<DiagnosticResponse> getAllDiagnosticsByUserId(Long userId){
+        userRepository.findById(userId)
+                      .orElseThrow(() -> new UserNotFoundException(userId));
+
+
+        List<DiagnosticRequest> diagnostics = diagnosticRequestRepository.findByUserId(userId);
+
+                return diagnostics.stream()
+                .map(diagnosticRequestMapper::toDto)
+                .toList();
+
+    }
+
+
+}
